01a1b206c7604f8cc5fb5b2f3e6f962997bec539
blin04 <ivanbokor17@gmail.com>
2022-03-29 13:07:30 +0200

    Wrote functions for loading images from the directory, however, for now it only works for loading from one directory

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import cv2
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 def winner(player1, player2):
     # igrac moze odigrati: "rock", "paper", "scissors"
     # moguci ishodi: 1, 2, 0
@@ -23,4 +29,46 @@ def winner(player1, player2):
             return 1
 
 
-print(winner("rock", "paper"))
+def load_images(folder, label):
+    img_data = []
+    class_name = []  # labels
+
+    for file in os.listdir(folder):
+        image_path = os.path.join(folder, file)
+        image = cv2.imread(image_path, cv2.COLOR_BGR2RGB)
+        # imamo sliku kao trodimenzionalni niz sa vrednostima od 0 do 255
+        image = np.array(image)
+        # pretvaramo vrednosti u interval [0, 1]
+        image = image.astype('float32')
+        image /= 255
+        img_data.append(image)
+        class_name.append(label)
+
+    return img_data, class_name
+
+
+def create_dataset(img_folder):
+    # ucitava slike i pravi od njih dataset
+    data = []
+    labels = []
+    # zanimaju nas slova A, B, i V
+
+    # privremena lista za skladistenje ucitanih slika, kako bi smo ih lakse
+    # spojili u dataset
+    for label in ["A"]:     # ovde bi trebalo biti ["A", "B", "V"]
+
+        path = img_folder + "/" + label
+        temp_data, temp_labels = load_images(path, label)
+
+        data = data + temp_data
+        labels = labels + temp_labels
+
+    return data, labels
+
+
+img_folder = "data/archive/asl_alphabet_train/asl_alphabet_train"
+data, labels = create_dataset(img_folder)
+
+plt.imshow(data[4])
+plt.show()
+
